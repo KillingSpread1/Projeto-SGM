703f9d164c6d5ed7c1fd7d003fec54b4d03bc6ca
KillingSpread1 <diogoamandio@hotmail.com>
2023-11-07 20:58:12 +0000

    Fixed the routes

--- routes/Albums.js
+++ routes/Albums.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/albums', function (req,res){ 
+router.get('/', function (req,res){ 
     res.render('albums.ejs')
 });
 
--- routes/Events.js
+++ routes/Events.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/events', function (req,res){ 
+router.get('/', function (req,res){ 
     res.render('events.ejs')
 });
 
