dd865e4083c3aaf91177cef7f10fd28533c25bd6
KillingSpread1 <2081020@student.uma.pt>
2023-11-29 20:34:09 +0000

    Implemented the button functionality of the page

--- routes/Events.js
+++ routes/Events.js
@@ -6,7 +6,7 @@ const Events = mongoose.model('Event');
 router.get('/', async function (req, res) { 
     try {
         const events = await Events.find({}).select('Name Description Type Videos Images');    
-        res.render('events', { events });
+        res.render('events', { events});
     } catch (err) {
         console.error(err);
         res.status(500).send('An error ocurred');
