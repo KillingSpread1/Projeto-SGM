c799c6d603328ce257b68e2a8637e583db660526
Fábio Nunes <fabiodiogo.an111@gmail.com>
2023-12-04 15:39:32 +0000

    header arranjado

--- routes/Members.js
+++ routes/Members.js
@@ -4,9 +4,11 @@ const mongoose = require('mongoose');
 const Members = mongoose.model('Members');
 
 router.get('/', async function (req, res) {
+    const isAuthenticated = !!req.session.userId;
+
     try {
         const members = await Members.find({});
-        res.render('members', { members });
+        res.render('members', { members, isAuthenticated });
     } catch (err) {
         console.error(err);
         res.status(500).send('An error ocurred');
