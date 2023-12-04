49ea4dfbee2886d7d36da387b77099cfc7d74472
FranciscoBelo18 <96338080+FranciscoBelo18@users.noreply.github.com>
2023-12-04 16:02:59 +0000

    Fixed setup discography page

--- routes/Discography.js
+++ routes/Discography.js
@@ -4,9 +4,10 @@ const mongoose = require('mongoose');
 const Discography = mongoose.model('Discography');
 
 router.get('/', async function (req, res) {
+    const isAuthenticated = !!req.session.userId;
     try {
         const albums = await Discography.find({});
-        res.render('discography', { albums });
+        res.render('discography', { albums, isAuthenticated });
     } catch (err) {
         console.error(err);
         res.status(500).send('An error ocurred');
